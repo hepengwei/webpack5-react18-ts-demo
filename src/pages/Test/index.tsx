4cdfb6c68542b4167afd7a6f04d0d7baa22fcf93
hepengwei <1007941801@qq.com>
2022-09-21 17:30:31 +0800

    添加好看的动效按钮组件

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import banner from "images/banner.png";
 import styles from "./index.module.less";
+import TiltButton from "@/components/TiltButton";
 
 const Test = () => {
   return (
     <div className={styles.container}>
-      <p className={styles.text}>测试页面地方個地方</p>
+      <p className={styles.text}>测试页面</p>
       <input />
       <img src={banner} alt="" />
+      <TiltButton
+        style={{ marginTop: "50px" }}
+      >
+        按 钮
+      </TiltButton>
     </div>
   );
 };
--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -2,3 +2,7 @@ declare module "*.module.less" {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module "react-tilt" {
+  export default Record<string, any>;
+}
